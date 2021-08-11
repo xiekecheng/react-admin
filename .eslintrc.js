fb84be79c480b70415700b7577f903db480c99db
xiekecheng <xkccoding@gmail.com>
2021-08-11 10:45:49 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,18 @@ module.exports ={
       "parserOptions": {
           "sourceType": "module",
           "allowImportExportEverywhere": true
-      }
+      },
+      "plugins": [
+        // ...
+        "react-hooks"
+      ],
+      "rules": {
+        // ...
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn"
+      },
+      extends: [
+        "react-app",
+        "react-app/jest"
+      ]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,32 @@ function App() {
 	const dispatch = useAppDispatch()
 	const token = useAppSelector(state => state.counter.token)
 	const user = useAppSelector(state => state.counter.user)
-	useHistory().replace('/login')
+	// useHistory().replace('/login')
 	// 有token,则用token获取user信息
 	useEffect(() => {
 		if (token) {
 			dispatch(getUserInfo(token))
 		}
 	}, [token])
-  useEffect(()=>{
-		if(!token){
+  // useEffect(()=>{
+	// 	if(!token){
 			
-		}
-  }, [token])
-	return (
-		<>
-		<BrowserRouter>
-			<Switch>
-				<Route path='/' component={LayoutPage}/>
-				<Route path='/login' component={Login}/>
-			</Switch>
-		</BrowserRouter>
-		</>
-	)
+	// 	}
+  // }, [token])
+
+
+	// return (
+	// 	<>
+	// 	<BrowserRouter>
+	// 		<Switch>
+	// 			<Route path='/' component={LayoutPage}/>
+	// 			<Route path='/login' component={Login}/>
+	// 		</Switch>
+	// 	</BrowserRouter>
+	// 	</>
+	// )
+
+	return token?(user.roleName? <LayoutPage/>:null):<Login/>
 }
 
 export default App
