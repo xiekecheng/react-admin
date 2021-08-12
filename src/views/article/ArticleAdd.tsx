ef1389cbcfa6813771141e1e8869c46651276f79
xiekecheng <xkccoding@gmail.com>
2021-08-12 10:28:49 +0800

    文章模块初步完成

---
+++ src/views/article/ArticleAdd.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BasicForm = () => {
+	return (
+		<div>
+			<h1>文章新增</h1>
+			
+		</div>
+	);
+};
+
+export default BasicForm;
---
+++ src/views/article/ArticleList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BasicForm = () => {
+	return (
+		<div>
+			<h1>文章列表</h1>
+			
+		</div>
+	);
+};
+
+export default BasicForm;
