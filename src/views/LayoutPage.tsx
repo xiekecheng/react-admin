02222832e06bcc54198d948e79fda075e1474e97
xiekecheng <xkccoding@gmail.com>
2021-08-21 17:57:29 +0800

    Update LayoutPage.tsx

--- src/views/LayoutPage.tsx
+++ src/views/LayoutPage.tsx
@@ -1,32 +1,36 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-11 11:10:57
+ * @LastEditors: xkccoding@gmail.com
+ * @LastEditTime: 2021-08-19 10:06:06
+ * @FilePath: /react-admin/src/views/LayoutPage.tsx
+ */
 import './style.scss'
 import React, { useCallback, useState } from 'react'
-import {  Route, Link, Switch } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
 import { Layout, Menu } from 'antd'
 const { SubMenu } = Menu
 import { useAppSelector } from '@/store/hooks'
 import goodsArr from '@/router'
-import {
-	MenuUnfoldOutlined,
-	MenuFoldOutlined
-} from '@ant-design/icons'
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
 const { Header, Sider, Content } = Layout
 // LayoutPage组件
 const LayoutPage = () => {
 	const [collapsed, setCollapsed] = useState(false)
-	const user = useAppSelector(state => state.user.user)
+	const user = useAppSelector((state) => state.user.user)
 
 	const toggle = () => {
 		setCollapsed(!collapsed)
 	}
 	// Sider栏菜单渲染函数
-	const renderMenu = arr => {
+	const renderMenu = (arr) => {
 		return arr.map(
-			item =>
+			(item) =>
 				item.permission.includes(user.role) && (
 					<SubMenu key={item.id} icon={item.icon} title={item.title}>
 						{item.children &&
 							item.children.map(
-								ele =>
+								(ele) =>
 									ele.permission.includes(user.role) && (
 										<Menu.Item key={ele.id}>
 											<Link to={ele.path}>{ele.title}</Link>
@@ -40,8 +44,8 @@ const LayoutPage = () => {
 
 	const renderRoute = useCallback((arr, flag) => {
 		let result: any = []
-		const recursion = arr => {
-			arr.map(ele => {
+		const recursion = (arr) => {
+			arr.map((ele) => {
 				if (!flag || ele.permission.includes(user.role)) {
 					result.push(
 						<Route
@@ -55,7 +59,7 @@ const LayoutPage = () => {
 				if (ele.children) recursion(ele.children)
 			})
 		}
-		arr.map(ele => {
+		arr.map((ele) => {
 			recursion(ele.children)
 		})
 		return result
@@ -63,36 +67,36 @@ const LayoutPage = () => {
 
 	return (
 		<>
-				<Layout>
-					<Sider trigger={null} collapsible collapsed={collapsed}>
-						<div className='logo'>
-							<img src='' alt='' />
-						</div>
-						<Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
-							{/* Sider菜单栏渲染 */}
-							{/* {user.role&&renderMenu(goodsArr)} */}
-							{renderMenu(goodsArr)}
-						</Menu>
-					</Sider>
-					<Layout className='site-layout'>
-						<Header className='site-layout-background' style={{ padding: 0 }}>
-							{React.createElement(
-								collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-								{
-									className: 'trigger',
-									onClick: toggle,
-								}
-							)}
-							用户名: <span>{user.roleName}</span>
-						</Header>
-						<Content className='site-layout-background'>
-							<Switch>
-								{/* 组件路由渲染 */}
-								{user.role && renderRoute(goodsArr, true)}
-							</Switch>
-						</Content>
-					</Layout>
+			<Layout>
+				<Sider trigger={null} collapsible collapsed={collapsed}>
+					<div className='logo'>
+						<img src='' alt='' />
+					</div>
+					<Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+						{/* Sider菜单栏渲染 */}
+						{/* {user.role&&renderMenu(goodsArr)} */}
+						{renderMenu(goodsArr)}
+					</Menu>
+				</Sider>
+				<Layout className='site-layout'>
+					<Header className='site-layout-background' style={{ padding: 0 }}>
+						{React.createElement(
+							collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+							{
+								className: 'trigger',
+								onClick: toggle,
+							}
+						)}
+						用户名: <span>{user.roleName}</span>
+					</Header>
+					<Content className='site-layout-background'>
+						<Switch>
+							{/* 组件路由渲染 */}
+							{user.role && renderRoute(goodsArr, true)}
+						</Switch>
+					</Content>
 				</Layout>
+			</Layout>
 		</>
 	)
 }
